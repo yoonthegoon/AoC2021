bfb27cb7e8e07077a56087f5e2e35f4d62af363f
Yunis <a@a.a>
2021-12-03 16:09:33 -0500

    solve day 3, resolve part 1 and solve part 2

--- days/day_03.py
+++ days/day_03.py
@@ -9,73 +9,35 @@ report = r.text.split('\n')[:-1]
 
 
 def part1():
-    gamma, epsilon = '', ''
+    gamma = ''.join(list(map(lambda i: str(int(sum([float(bits[i]) for bits in report]) / len(report) + 0.5)),
+                             range(len(report[0])))))
+    epsilon = ''.join('0' if int(i) else '1' for i in gamma)
 
-    bits = dict()
-    for i in 'abcdefghijkl':
-        bits[i] = []
-
-    for i in report:
-        for j, k in enumerate(i):
-            bits['abcdefghijkl'[j]].append(float(k))
-
-    for key in bits.keys():
-        bits[key] = sum(bits[key]) / len(bits[key])
-        bits[key] = int(bits[key] + .5)
-
-        gamma += str(bits[key])
-        epsilon += '0' if bits[key] else '1'
-
-    gamma = int(gamma, 2)
-    epsilon = int(epsilon, 2)
+    gamma, epsilon = int(gamma, 2), int(epsilon, 2)
 
     return gamma * epsilon
 
 
 def part2():
-    report = [
-        '00100',
-        '11110',
-        '10110',
-        '10111',
-        '10101',
-        '01111',
-        '00111',
-        '11100',
-        '10000',
-        '11001',
-        '00010',
-        '01010'
-    ]
-
-    o2, co2 = '', ''
-
     o2_report, co2_report = report, report
 
-    for i in range(5):
+    for i in range(len(o2_report[0])):
         if len(o2_report) == 1:
             break
 
-        common = '1' if sum(float(j[i]) for j in o2_report) / len(o2_report) + .5 >= 0.5 else '0'
-        temp_o2 = o2_report
-        for j in temp_o2:
-            if j[i] != common:
-                o2_report.remove(j)
-            print(o2_report)
-        o2 += common
+        o2_report = list(filter(lambda j: j[i] == str(int(sum(map(
+            lambda k: float(k[i]), o2_report)) / len(o2_report) + 0.5)), o2_report))
 
-    # for i in range(5):
-    #     uncommon = '0' if sum(float(j[i]) for j in co2_report) / len(co2_report) >= 0.5 else '1'
-    #     for j in co2_report:
-    #         if j[i] != uncommon:
-    #             co2_report.remove(j)
-    #
-    #     co2 += uncommon
-    print(o2, co2)
-    # o2 = int(o2, 2)
-    # co2 = int(co2, 2)
-    #
-    # return o2 * co2
+    for i in range(len(co2_report)):
+        if len(co2_report) == 1:
+            break
+
+        co2_report = list(filter(lambda j: j[i] != str(int(sum(map(
+            lambda k: float(k[i]), co2_report)) / len(co2_report) + 0.5)), co2_report))
+
+    o2, co2 = int(o2_report[0], 2), int(co2_report[0], 2)
+
+    return o2 * co2
 
 
 if __name__ == '__main__':
