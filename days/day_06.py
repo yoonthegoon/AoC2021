093d0530d6036425121f2cb01e42ed95ad499a4c
Yunis <a@a.a>
2021-12-06 19:18:18 -0500

    solve day 6, part 2

--- days/day_06.py
+++ days/day_06.py
@@ -5,29 +5,34 @@ import requests
 cookie = open('.config', 'r').read()
 r = requests.get('https://adventofcode.com/2021/day/6/input', cookies=dict(session=cookie))
 
-# population = [int(i) for i in r.text.replace('\n', '').split(',')]
-# population = [3, 4, 3, 1, 2]
-population = [6]
+population = [int(i) for i in r.text.replace('\n', '').split(',')]
 
 
 def part1():
-    pop1 = population.copy()
+    pop = population.copy()
 
     for _ in range(80):
-        for i, fish in enumerate(pop1):
+        for i, fish in enumerate(pop):
             if not fish:
-                pop1[i] = 6
-                pop1.append(9)
+                pop[i] = 6
+                pop.append(9)
             else:
-                pop1[i] -= 1
+                pop[i] -= 1
 
-    return len(pop1)
+    return len(pop)
 
 
 def part2():
-    pop2 = population.copy()
+    pop = [0]*9
+    for i in population:
+        pop[i] += 1
 
-    pass
+    for _ in range(256):
+        pop.append(pop[0])
+        pop[7] += pop[0]
+        pop.pop(0)
+
+    return sum(pop)
 
 
 if __name__ == '__main__':
